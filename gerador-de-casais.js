a294776f8fe80a58082035da13096fa014fbb5be
johnlimaa <johnlimaa@hotmail.com>
2019-11-07 21:33:48 -0300

    Geração de casais do mesmo sexo.

--- gerador-de-casais.js
+++ gerador-de-casais.js
@@ -1,8 +1,16 @@
 let couplesQuantity = 30;
 let homosexualCouple = true;
 
-function generateAge() {
+function generateAge(age) {
+    let lowerAge = 18, higherAge = 80;
     
+    if (typeof age === 'number') {
+        console.log('number');
+    } else if (typeof age === 'undefined') {
+        console.log('vazio');
+        return 10;
+    };
+
 };
 
 function generateNames(firstName, lastName) {
@@ -18,35 +26,51 @@ function generateNames(firstName, lastName) {
 };
 
 function generateCouples(couples, homosexual) {
-    let maleNames = ["Bernardo", "Matheus", "Davi", "Heitor", "Henrique", "Bruno", "Samuel", "Felipe", "Lorenzo", "Benjamim", "Vinícius", "Eduardo", "Noah", "Antônio", "Diego", "Leonardo", "Nicolas", "Daniel", "Thiago", "José", "Alexandre", "Gael", "André", "Vitor", "Mateus", "Carlos", "Caleb", "Paulo", "Theo", "Caio", "Joaquim", "Marcos", "Bryan", "Pietro", "Tiago", "Victor", "Francisco", "Yuri", "Fernando", "Ruan"];
+    let maleNames = ["Bernardo", "Matheus", "Davi", "Heitor", "Henrique", "Bruno", "Samuel", "Felipe", "Lorenzo", "Benjamim", "Vinícius", "Eduardo", "Noah", "Antônio", "Diego", "Leonardo", "Nicolas", "Daniel", "Thiago", "José", "Alexandre", "Gael", "André", "Vitor", "Mateus", "Carlos", "Caleb", "Paulo", "Theo", "Caio", "Joaquim", "Marcos", "Bryan", "Pietro", "Tiago", "Victor", "Francisco", "Yuri", "Fernando", "Ruan"],
+    femaleNames = ["Helena", "Isabela", "Lara", "Camila", "Letícia", "Valentina", "Luana", "Amanda", "Yasmin", "Sophia", "Juliana", "Bruna", "Cecília", "Fernanda", "Isadora", "Lorena", "Lívia", "Manuela", "Vitória", "Sara", "Aline", "Luiza", "Luna", "Gabriela", "Giovanna", "Jéssica", "Bianca", "Melissa", "Carolina", "Ester", "Regina", "Vanessa", "Heloísa", "Rafaela", "Nicole", "Milena", "Isabella", "Laís", "Ana Clara", "Eloá"],
+    lastNames = ["Abravanel", "Albuquerque", "Alencar", "Almada", "Álvares", "Alves", "Amoreira", "Andrade", "Annenberg", "Antena", "Barroso", "Bastos", "Beltrão", "Bentes", "Bernardes", "Bernardi", "Bial", "Bittencourt", "Bittencourt", "Boaventura", "Bonfim", "Carreira", "Carvalheira", "Carvalho", "Coutinho", "Crespo", "D’Ávila", "da Costa", "da Cruz", "da Cunha", "da Fonseca", "da Fontoura", "da Fronteira", "da Gama", "da Luz", "da Madureira", "da Maia", "da Rosa", "da Silva", "da Silveira", "da Veiga", "Dantas", "Dantas", "das Neves", "de Assunção", "de Borba", "de Lara", "de Lins", "de Oliveira", "de Quadros", "de Sá", "Dias", "Diegues", "do Prado", "Henriques", "Hermingues", "Hernandes", "Laranjeira", "Louzada", "Macieira", "Madeira", "Maldonado", "Mangueira", "Marcondes", "Marinho", "Marins", "Ribeira", "Rodrigues", "Sales", "Soares", "Soeira", "Solimões", "Steves", "Tavares", "Teixeira", "Teles", "Torres", "Trindade", "Vargas", "Vasconcelos", "Vasques"];
 
-    let femaleNames = ["Helena", "Isabela", "Lara", "Camila", "Letícia", "Valentina", "Luana", "Amanda", "Yasmin", "Sophia", "Juliana", "Bruna", "Cecília", "Fernanda", "Isadora", "Lorena", "Lívia", "Manuela", "Vitória", "Sara", "Aline", "Luiza", "Luna", "Gabriela", "Giovanna", "Jéssica", "Bianca", "Melissa", "Carolina", "Ester", "Regina", "Vanessa", "Heloísa", "Rafaela", "Nicole", "Milena", "Isabella", "Laís", "Ana Clara", "Eloá"];
-
-    let lastNames = ["Abravanel", "Albuquerque", "Alencar", "Almada", "Álvares", "Alves", "Amoreira", "Andrade", "Annenberg", "Antena", "Barroso", "Bastos", "Beltrão", "Bentes", "Bernardes", "Bernardi", "Bial", "Bittencourt", "Bittencourt", "Boaventura", "Bonfim", "Carreira", "Carvalheira", "Carvalho", "Coutinho", "Crespo", "D’Ávila", "da Costa", "da Cruz", "da Cunha", "da Fonseca", "da Fontoura", "da Fronteira", "da Gama", "da Luz", "da Madureira", "da Maia", "da Rosa", "da Silva", "da Silveira", "da Veiga", "Dantas", "Dantas", "das Neves", "de Assunção", "de Borba", "de Lara", "de Lins", "de Oliveira", "de Quadros", "de Sá", "Dias", "Diegues", "do Prado", "Henriques", "Hermingues", "Hernandes", "Laranjeira", "Louzada", "Macieira", "Madeira", "Maldonado", "Mangueira", "Marcondes", "Marinho", "Marins", "Ribeira", "Rodrigues", "Sales", "Soares", "Soeira", "Solimões", "Steves", "Tavares", "Teixeira", "Teles", "Torres", "Trindade", "Vargas", "Vasconcelos", "Vasques"];
-
-    let couplesList = [],
-        age;
+    let couplesList = [];        
 
     for(let i = 0; i < couples; i++) {
         if (homosexual) {
-            age =  generateAge();
-            console.log(age[0]);
-            console.log(age[1]);
-        } else {
-            age = generateAge();
+            if(Math.floor(Math.random() * 2) ? true : false) {
+                couplesList.push({
+                    firstPerson: generateNames(maleNames, lastNames),
+                    secondPerson: generateNames(femaleNames, lastNames),
+                    ageFirstPerson: generateAge(),
+                    ageSecondPerson: generateAge(this.ageFirstPerson)
+                });
+            } else {
+                if(Math.floor(Math.random() * 2) ? true : false) {
+                    couplesList.push({
+                        firstPerson: generateNames(maleNames, lastNames),
+                        secondPerson: generateNames(maleNames, lastNames),
+                        ageFirstPerson: generateAge(),
+                        ageSecondPerson: generateAge(this.ageFirstPerson)
+                    });
+                } else {
+                    couplesList.push({
+                        firstPerson: generateNames(femaleNames, lastNames),
+                        secondPerson: generateNames(femaleNames, lastNames),
+                        ageFirstPerson: generateAge(),
+                        ageSecondPerson: generateAge(this.ageFirstPerson)
+                    });
+                }
+            }  
+        } else {            
             couplesList.push({
                 firstPerson: generateNames(maleNames, lastNames),
                 secondPerson: generateNames(femaleNames, lastNames),
-                ageFirstPerson: age[0],
-                ageSecondPerson: age[1]
+                ageFirstPerson: generateAge(),
+                ageSecondPerson: generateAge(this.ageFirstPerson)
             });
         }
-        console.log(Math.floor(Math.random() * 2) ? true : false);        
-        console.log(generateNames(maleNames, lastNames));
+        // console.log(Math.floor(Math.random() * 2) ? true : false);        
+        // console.log(generateNames(maleNames, lastNames));
     };
-    
+    console.log(couplesList);
     return couplesList;
 };
 
-generateCouples(couplesQuantity, homosexualCouple);
-
+console.log(generateCouples(couplesQuantity, homosexualCouple));
